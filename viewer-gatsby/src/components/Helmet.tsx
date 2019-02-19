639d674511767222599e5e4bc0235814862d1ea0
c10t <c10t@fa.ke>
2019-02-19 23:10:46 +0900

    introduce Railway Font

--- viewer-gatsby/src/components/Helmet.tsx
+++ viewer-gatsby/src/components/Helmet.tsx
@@ -6,6 +6,7 @@ const MyHelmet: React.FC = () => (
     <meta charSet="utf-8" />
     <title>Data Viewer</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css"></link>
   </Helmet>
 )
 
