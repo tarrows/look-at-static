d74407bf95cd6afd7f7b52c95d4fc58a2c5ca8a5
c10t <c10t@fa.ke>
2019-02-25 22:51:12 +0900

    remove unused files

--- my-story/.storybook/config.js
+++ my-story/.storybook/config.js
@@ -3,7 +3,6 @@ import { configure } from '@storybook/react';
 const req = require.context('../src/components', true, /\.stories\.tsx$/);
 
 function loadStories() {
-  // require('../src/stories');
   req.keys().forEach(filename => req(filename));
 }
 
