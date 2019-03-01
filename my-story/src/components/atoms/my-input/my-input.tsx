35612bc358426bdeb06b0879009edcbe50bb7715
c10t <c10t@fa.ke>
2019-03-01 09:13:12 +0900

    tweak input field

--- my-story/src/components/atoms/my-input/my-input.tsx
+++ my-story/src/components/atoms/my-input/my-input.tsx
@@ -6,15 +6,17 @@ const Wrapper = styled.div`
 `
 
 const Input = styled.input`
+  border: 1px solid rgba(147, 128, 108, 0.25);
+  padding: 0.5em 0.75em;
   flex: 1;
   border-radius: 0 2px 2px 0;
 `
 
 const Prefix = styled.span`
-  background-color: rgba(147, 128, 108, 0.1);
-  color: #666666;
   border: 1px solid rgba(147, 128, 108, 0.25);
   padding: 0.5em 0.75em;
+  background-color: rgba(147, 128, 108, 0.1);
+  color: #666666;
   border-radius: 2px 0 0 2px;
 `
 
