8b7a86a1f00d19eca6e3b89d591b80eb8bd894be
c10t <c10t@fa.ke>
2019-03-08 23:34:27 +0900

    introduce styled-components and sanitize.css

--- mock-next/src/components/organisms/navbar/navbar.tsx
+++ mock-next/src/components/organisms/navbar/navbar.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
-const Navbar: React.FC = () => (<p>Navbar</p>)
+const Button = styled.button`
+  width: 300px;
+  background-color: "#e6e6bb";
+`
+
+const Navbar: React.FC = () => (<p><Button>Navbar</Button></p>)
 
 export default Navbar
