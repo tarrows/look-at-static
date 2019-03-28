eb5e3b54e6e4a438ebd743cef3033bbd53c974f6
c10t <c10t@fa.ke>
2019-03-28 09:17:53 +0900

    delete unused files

--- hello-gatsby/src/components/molecules/record-input/index.tsx
+++ hello-gatsby/src/components/molecules/record-input/index.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
 
 const RecordInput: React.FC = () => (
-  <form action="#">
-    <div className="mdl-textfield mdl-js-textfield">
-      <input className="mdl-textfield__input" type="text" />
-      <label className="mdl-textfield__label" >Key...</label>
-    </div>
-    <div className="mdl-textfield mdl-js-textfield">
-      <input className="mdl-textfield__input" type="text" />
-      <label className="mdl-textfield__label" >Val...</label>
-    </div>
-  </form>
+  <div className="mdl-grid">
+    <form action="#">
+      <div className="mdl-textfield mdl-js-textfield">
+        <input className="mdl-textfield__input" type="text" />
+        <label className="mdl-textfield__label" >Key...</label>
+      </div>
+      <div className="mdl-textfield mdl-js-textfield">
+        <input className="mdl-textfield__input" type="text" />
+        <label className="mdl-textfield__label" >Val...</label>
+      </div>
+    </form>
+  </div>
 )
 
 export default RecordInput
--- hello-gatsby/src/services/root-reducer.ts
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux'
-import storageReducer from './web-storage/reducer'
-
-export default combineReducers({ storageReducer })
