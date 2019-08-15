14176d3d0df43384c2db499a15b6424c6653bacd
c10t <c10t@fa.ke>
2019-08-15 23:59:24 +0900

    parcel

---
+++ parcel-react/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import IndexPage from './templates/index-page'
+
+ReactDOM.render(<IndexPage />, document.getElementById('root'))
---
+++ parcel-react/src/templates/index-page.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const IndexPage: React.FC = () => <h1>Hello, Parcel!</h1>
+
+export default IndexPage
